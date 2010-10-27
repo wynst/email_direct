182150d9df214fe70688fbef64f4a643b8bd2173
wynst <wynst.uei@gmail.com>
2010-10-27 18:13:22 +0700

    clean up sourcespec

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,53 +1,55 @@
 require 'spec_helper'
 require 'email_direct/soap'
 
-#context "A live emaildirect SOAP consumer"
-
 describe EmailDirect::SOAP, "#source" do
-  before do
-    @soap = EmailDirect::SOAP.new(ENV['USERNAME'], ENV['PASSWORD'])
-    @soap.debug = true
-  end
-  it "interacts with source" do
-    puts "Element should already exists.."
-    element_name = 'RubyTestSource'
-    description  = 'a test source item'
-    result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
-    result.success?.should == false
-    result.code.should     == 402
-    result.message.should  == "A Source with this name already exists."
 
-    puts "creating new source.."
-    element_name = 'RubyTestSource2'
-    description  = 'a test source item'
-    result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
-    result.success?.should == true
+  describe "A live EmailDirect Source actions"
+    before do
+      @soap = EmailDirect::SOAP.new(ENV['USERNAME'], ENV['PASSWORD'])
+      @soap.debug = true
+    end
+    it "interacts with source" do
+      puts "Element should already exists.."
+      element_name = 'RubyTestSource'
+      description  = 'a test source item'
+      result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
+      result.success?.should == false
+      result.code.should     == 402
+      result.message.should  == "A Source with this name already exists."
 
-    puts "find the added item"
-    result = @soap.Source_GetAll()
-    result.success?.should == true
+      puts "creating new source.."
+      element_name = 'RubyTestSource2'
+      description  = 'a test source item'
+      result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
+      result.success?.should == true
 
-    source = result.data["Element"].find {|e| e["ElementName"] == element_name}
-    source.should_not == nil
+      puts "find the added item"
+      result = @soap.Source_GetAll()
+      result.success?.should == true
 
-    puts "we're done with the test, delete the test source"
-    result = @soap.Source_Delete("SourceID" => source["ElementID"])
-    result.success?.should == true
+      source = result.data["Element"].find {|e| e["ElementName"] == element_name}
+      source.should_not == nil
 
-  end
-end
+      puts "we're done with the test, delete the test source"
+      result = @soap.Source_Delete("SourceID" => source["ElementID"])
+      result.success?.should == true
 
-describe "An invalid authentication error in soap response with Source_GetAll" do
-  before do
-    @soap = EmailDirect::SOAP.new('invalid', 'password')
-    @soap.debug = true
+    end
   end
 
-  it "fails gracefully" do
-    result = @soap.Source_GetAll()
-    result.success?.should == false
-    result.code.should     == 10
-    result.message.should  == "Invalid AccountName or Password."
+  describe "An invalid authentication error in soap response with Source_GetAll" do
+    before do
+      @soap = EmailDirect::SOAP.new('invalid', 'password')
+      @soap.debug = true
+    end
+
+    it "fails gracefully" do
+      result = @soap.Source_GetAll()
+      result.success?.should == false
+      result.code.should     == 10
+      result.message.should  == "Invalid AccountName or Password."
+    end
   end
+
 end
 
