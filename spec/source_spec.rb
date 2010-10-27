c522882d36de0152a30b69ae66d679f6ba1c4f01
wynst <wynst.uei@gmail.com>
2010-10-27 17:00:13 +0700

    add source spec for Source_GetAll & Source_Delete

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -3,33 +3,36 @@ require 'email_direct/soap'
 
 #context "A live emaildirect SOAP consumer"
 
-describe EmailDirect::SOAP, "#source_add" do
+describe EmailDirect::SOAP, "#source" do
   before do
     @soap = EmailDirect::SOAP.new(ENV['USERNAME'], ENV['PASSWORD'])
     @soap.debug = true
   end
   it "interacts with source" do
-    #create new source
+    puts "Element should already exists.."
+    element_name = 'RubyTestSource'
+    description  = 'a test source item'
+    result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
+    result.success?.should == false
+    result.code.should     == 402
+    result.message.should  == "A Source with this name already exists."
+
+    puts "creating new source.."
     element_name = 'RubyTestSource2'
     description  = 'a test source item'
-    result = @soap.Source_Add("ElementName" => "RubyTestSource", "Description" => 'a test source item')
+    result = @soap.Source_Add("ElementName" => element_name, "Description" => element_name)
     result.success?.should == true
 
-    #find the added item
+    puts "find the added item"
     result = @soap.Source_GetAll()
+    result.success?.should == true
 
+    source = result.data["Element"].find {|e| e["ElementName"] == element_name}
+    source.should_not == nil
 
-    result = @soap.Source_Delete()
-#    ap result
-#    result.code.should == '201'
-#    result.description.should == 'blabla'
-
-#    #server should complain since element already exist
-#    result = @api.source_add(element_name, description)
-#    result.code.should == '201'
-#    result.description.should == ''
-
-    #we're done with the test, delete the test source
+    puts "we're done with the test, delete the test source"
+    result = @soap.Source_Delete("SourceID" => source["ElementID"])
+    result.success?.should == true
 
   end
 end
